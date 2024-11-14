e0cefd6658b7b1da7cbc1473a9a61fb538d650b8
Paulo-21 <paul.cibier@gmail.com>
2024-11-14 20:59:46 +0100

    Add other bench

--- benches/bench_tree.rs
+++ benches/bench_tree.rs
@@ -3,6 +3,8 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use cartesien_tree::insert_tonnes;
 pub fn criterion_benchmark(c: &mut Criterion) {        
     c.bench_function("bench_tree", |b| b.iter(|| insert_tonnes(black_box( 1000))));
+    c.bench_function("bench_tree", |b| b.iter(|| insert_tonnes(black_box( 10000))));
+    c.bench_function("bench_tree", |b| b.iter(|| insert_tonnes(black_box( 100000))));
 }
 
 criterion_group!(benches, criterion_benchmark);
